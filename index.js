25b0efa7619379932820b1f4a02e338fc3db2165
radville <laura.radville@gmail.com>
2019-10-31 13:30:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + 
+    string.slice(1)
+  }
+  
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+    let words = string.split(" ");
+    let noCaps = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+
+    function selectiveCapitalization(word) {
+      if (noCaps.includes(word)) {
+        return word
+      } else {
+        return this.capitalize(word)
+      }
+    }
+    return words.map(selectiveCapitalization).join(" ")
+  }
+
 }
