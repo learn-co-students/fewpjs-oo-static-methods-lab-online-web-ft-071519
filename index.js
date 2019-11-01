2030c35e7856214e09901a990202593e9ac1f276
Audrea Cook <audreacwork@gmail.com>
2019-11-01 16:36:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+    static capitalize(string) {
+        const first = string[0].toUpperCase();
+        const rest = [...string].slice(1).join('');
+        return first + rest
+    }
+
+    static sanitize(string) {
+        return string.replace(/[^A-Za-z0-9-\s']+/g, '')
+    }
+
+    static titleize(string) {
+        const lowerArray = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+        let array = string.split(' ');
+        const titleArray = [this.capitalize(array[0])];
+        array = array.slice(1);
+
+        for (const word of array) {
+            if (!lowerArray.find(w => w === word)) {
+                titleArray.push(this.capitalize(word))
+            } else {
+                titleArray.push(word)
+            }
+        }
+
+        return titleArray.join(' ')
+    }
+
 }
