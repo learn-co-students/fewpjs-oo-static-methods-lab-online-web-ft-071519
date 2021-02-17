a7060ee3933e36c487a46386cac75ee2c3881026
Takeshi Kimoto <lnuk2009jp@gmail.com>
2021-02-17 07:19:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+
+let str = "hi, i am tashenko. nice to meet you"
+let arr = [1,2,3]
+
 class Formatter {
   static capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1)
+    return str.replace(str[0], str[0].toUpperCase())
   }
   static sanitize(str){
-    return str.replace(/[^A-Za-z0-9 '-]+/g, '' )
+    return str.replace(/[^A-Za-z0-9 '-]+/g, '')
   }
 
   static titleize(str){
-    let arr = str.split(" ")
-    arr.forEach (function (ele){
-      return this.capitalize(ele)
+    const smalls = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    return str.split(" ").map((w, i) => {
+      if (!smalls.includes(w)) {
+        return Formatter.capitalize(w)
+      } else {
+        return i == 0 ? Formatter.capitalize(w) : w 
+      }
+    }).join(" ")
+
+  }
+
+  static test(arr){
+    arr.forEach(function(ele){
+      return ele * ele
     })
   }
 }
 
-let str = "hi, i am takeshi. nice to meet you"
